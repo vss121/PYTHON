1e45a85066ee1950a7a8ea9108bec08d1eadca2e
vss121 <yk_kim@chungbuk.ac.kr>
2022-07-07 23:48:07 +0900

    add files (7/7)

--- 알고리즘 문제풀이/section 3/01 회문 문자열 검사.py
+++ 알고리즘 문제풀이/section 3/01 회문 문자열 검사.py
@@ -1,14 +1,21 @@
 import sys
 sys.stdin=open("C:/Users/ykkim/github/PYTHON/알고리즘 문제풀이/section 3/input.txt","rt")
+arr = []
+num = 0
+yaksu = 0
+s = input()
+for i in s:
+    if i.isnumeric():
+        arr.append(i)
 
-n = int(input())
-for i in range(n):
-    s=input()
-    s = s.lower()
-    size = len(s)
-    for j in range(size//2):   # 문자열 안에서 문자를 직접 비교
-        if s[j]!=s[-1-j]:   # list의 index 접근을 뒤부터 -1, -2, ...로도 가능
-            print(f"#{i+1} NO") # print("#%d NO" % (i+1))
-            break
-    else:
-        print(f"#{i+1} YES")
+for j in range(len(arr)):
+    num += int(arr[-1*(j+1)])*(10**j)
+
+#print(arr)
+print(num)
+
+for k in range(1,num+1):
+    if num%k==0:
+        yaksu+=1
+
+print(yaksu)
