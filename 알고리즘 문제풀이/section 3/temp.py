c53a1d5c783a8837a8216d4c26a4c5eb40f63afd
vss121 <yk_kim@chungbuk.ac.kr>
2022-07-13 22:29:35 +0900

    22/07/13

--- 알고리즘 문제풀이/section 3/temp.py
+++ 알고리즘 문제풀이/section 3/temp.py
@@ -1,10 +1,10 @@
 import sys
 sys.stdin=open("C:/Users/ykkim/github/PYTHON/알고리즘 문제풀이/section 3/input.txt","rt")
-arr=list(range(21))
-for _ in range(10):
-    a,b= map(int, input().split())
-    for i in range((b-a+1)//2):
-        arr[a+i], arr[b-i] = arr[b-i], arr[a+i]
-a.pop(0)    # 0번 index를 pop
-for x in arr:
-    print(x, end=' ')
+n = int(input())
+a = list(map(int, input().split()))
+m = int(input())
+b = list(map(int, input().split()))
+
+c=a+b
+c.sort()
+print(c)
