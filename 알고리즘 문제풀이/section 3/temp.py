43e90c5a3874e2e75bd11642582353b06a67b228
vss121 <yk_kim@chungbuk.ac.kr>
2022-07-15 23:45:24 +0900

    22/07/15

--- 알고리즘 문제풀이/section 3/temp.py
+++ 알고리즘 문제풀이/section 3/temp.py
@@ -1,10 +1,14 @@
 import sys
-sys.stdin=open("C:/Users/ykkim/github/PYTHON/알고리즘 문제풀이/section 3/input.txt","rt")
-n = int(input())
+#sys.stdin=open("C:/Users/ykkim/github/PYTHON/알고리즘 문제풀이/section 3/input.txt","rt")
+n,m =map(int, input().split())
+cnt=0
 a = list(map(int, input().split()))
-m = int(input())
-b = list(map(int, input().split()))
-
-c=a+b
-c.sort()
-print(c)
+print(a)
+for i in range(n):
+    print(i)
+    sum=0
+    while(sum<m):
+        sum+=a[i]
+        i+=1
+    if sum==m:
+        cnt+=1
