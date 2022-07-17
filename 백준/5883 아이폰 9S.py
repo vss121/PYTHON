da06283fee77637ff2f3de3d501bf1a6ef4b4091
vss121 <yk_kim@chungbuk.ac.kr>
2022-07-17 23:15:28 +0900

    22/07/17

--- 백준/5883 아이폰 9S.py
+++ 백준/5883 아이폰 9S.py
@@ -1,9 +1,34 @@
 n=int(input())
-b=list()
-for i in range(n):
-    b[i]=int(input())
+a=list()
+for _ in range(n):
+    a.append(int(input()))
+a_set = set(a)
 
-b_set = set(b)
-cnt=1
-num=b[0]
-###############################
+res=0
+for x in a_set:
+    cnt=1
+    prev=a[0]
+    '''
+    for i in range(1,n):
+        if a[i]!=x and a[i]==prev:
+            cnt+=1
+        if a[i]!=x and a[i]!=prev:
+            prev=a[i]
+            cnt=1  ###
+        #print(cnt)'''
+             
+    for i in range(1,n):
+        if x==a[i]:
+            continue
+        if prev==a[i]:
+            cnt+=1
+        else:
+            prev=a[i]
+            cnt=1
+        
+        if cnt>res:####
+            res=cnt
+
+    #print(cnt)
+
+print(res)
